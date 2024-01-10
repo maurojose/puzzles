a8a34358c15b994efcd80be479ea2bc9b56d3c91
MauroJ <maurojsoares@gmail.com>
2024-01-10 13:02:25 +0000

    colocando as regras pra testar com usuários

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+      <h1>bem vindo</h1>
+      <div className="container mx-10">
+        <p>Este jogo está nas primeiras fases de desenvolvimento. É um jogo simples, com um único objetivo: seja o primeiro a completar o quebra-cabeça. Esse jogo tem o seguinte funcionamento:<br/>
+          - O primeiro a completar o quebra-cabeça será o vencedor e ganhará o prêmio;<br/>
+          - As peças podem ser compradas a partir de uma unidade. Mas o jogo te dará peças aleatórias e as vezes poderá te dar uma peça que você já tem;<br/>
+          - No futuro você poderá trocar peças com outros usuários, mas por enquanto isso nao ta funcionando.<br/>
+        </p>
+      </div>
     </main>
   );
 }
